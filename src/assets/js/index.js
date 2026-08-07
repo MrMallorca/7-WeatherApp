29f2b7d599fe07766f5d1d75a70a94535da3a5d3
MrMallorca <37783709+MrMallorca@users.noreply.github.com>
2026-08-07 20:09:06 +0200

    navbar carga en los demas slides

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,6 +3,8 @@ import '../css/styles.css';
 
 //Import js
 import loadHomePage from '../../slides/home.js'; 
+import loadNavBarPage from '../../components/navBar.js'; 
 
+loadNavBarPage();
 
 loadHomePage();
