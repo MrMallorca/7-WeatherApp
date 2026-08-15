f41f560c7221368c20c71909ac809d55e5508627
MrMallorca <37783709+MrMallorca@users.noreply.github.com>
2026-08-15 22:36:27 +0200

    arreglo para usar githubPages

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-  mode: 'development',
-  entry: './src/assets/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    static: './dist',
-    port: 3000,
-    open: true,
-    hot: true
-  }
+    mode: 'development',
+
+    entry: './src/assets/js/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'docs'),
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    devServer: {
+        static: './docs',
+        port: 3000,
+        open: true,
+        hot: true
+    }
 };
